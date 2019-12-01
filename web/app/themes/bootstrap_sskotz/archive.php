9ec60e5d2eeca3e8d88b572831dd9d9245ce38b9
Isac Santana <koushirogamer@gmail.com>
2019-12-01 19:59:41 -0300

    Some adjustments
    -Pages 'cosmos' and 'characters' excluded and passed for archives;
    -Modifications in menu structure

--- web/app/themes/bootstrap_sskotz/archive.php
+++ web/app/themes/bootstrap_sskotz/archive.php
@@ -17,49 +17,95 @@
 
 $templates = array('archive.twig', 'index.twig');
 $context = Timber::context();
+$timber_post = new Timber\Post();
+$archive = str_replace('/', '', $_SERVER['REQUEST_URI']);
 $posts = [];
-if (str_replace('/', '', $_SERVER['REQUEST_URI']) != 'news') {
-	$category = get_the_category();
-	if (!empty($category)) {
-		$context['categorypage'] = $category[0]->name;
+
+if ($timber_post->post_type == 'cosmos') {
+	$cosmos = [];
+	$args = array(
+		'post_type' => 'cosmos',
+		'post_satus' => 'publish',
+		'numberposts' => -1
+	);
+	$context['cosmos'] = Timber::get_posts($args);
+	foreach ($context['cosmos'] as $cosmo) {
+		$data = get_fields($cosmo->ID);
+		$info = [
+			'cosmo_name' => $data["cosmo_name"],
+			'cosmo_bonus' => $data["cosmo_bonus"],
+			'cosmo_qntstatus' => $data["cosmo_qntstatus"],
+			'cosmo_img' => $data["cosmo_img"]['url'],
+			'cosmo_type' => get_the_terms($cosmo->ID, 'cosmo_type')[0]->slug,
+			'cosmo_link' => get_permalink($cosmo->ID),
+			'cosmo_status1_tipo' => $data['cosmo_status1']['tipo'],
+			'cosmo_status1_max' => $data['cosmo_status1']['max'],
+		];
+		if ($data['cosmo_qntstatus'] > 1) {
+			$info['cosmo_status2_tipo'] = $data['cosmo_status2']['tipo'];
+			$info['cosmo_status2_max'] = $data['cosmo_status2']['max'];
+		}
+		$posts[] = array_merge($info);
+	};
+} else if ($timber_post->post_type == 'characters') {
+	$args = array(
+		'post_type' => 'characters',
+		'post_satus' => 'publish',
+		'numberposts' => -1
+	);
+	$context['post'] = Timber::get_posts($args);
+	foreach ($context['post'] as $saint) {
+		$data = get_fields($saint->ID);
+		$info = [
+			'character_name' => $data["character_name"],
+			'character_rarity' => $data["character_rarity"],
+			'character_thumb' => $data["character_thumb"]["url"],
+			'character_tier' => $data["character_tier"],
+			'character_link' => get_permalink($saint->ID),
+		];
+		$posts[] = array_merge($info);
+	};
+} else {
+	if ($archive != 'news') {
+		$category = get_the_category();
+		if (!empty($category)) {
+			$context['categorypage'] = $category[0]->name;
+			$args = array(
+				'post_type' => 'post',
+				'post_satus' => 'publish',
+				'posts_per_page' => 15,
+				'orderby' => 'publish_date',
+				'cat' => $category[0]->cat_ID,
+			);
+		}
+	} else {
+		$context['categorypage'] = 'Publicações';
 		$args = array(
 			'post_type' => 'post',
 			'post_satus' => 'publish',
 			'posts_per_page' => 15,
 			'orderby' => 'publish_date',
-			'cat' => $category[0]->cat_ID,
 		);
 	}
-} else {
-	$context['categorypage'] = 'Publicações';
-	$args = array(
-		'post_type' => 'post',
-		'post_satus' => 'publish',
-		'posts_per_page' => 15,
-		'orderby' => 'publish_date',
-		'paged' => $paged
-	);
+	if (!empty($args)) {
+		$context['news'] = new Timber\PostQuery($args);
+		foreach ($context['posts'] as $post) {
+			$data = get_fields($post->ID);
+			$public = [
+				'post_title' => $post->post_title,
+				'post_img' => $data["post_thumb"]['url'],
+				'post_author_name' => get_the_author_meta($post->post_author),
+				'post_author_avatar' => get_avatar_url($post->post_author),
+				'post_category' => get_the_category($post->ID),
+				'post_desc' => mb_strimwidth($data["post_desc"], 0, 100, "..."),
+				'post_date' => date("d/m/Y H:i:s", strtotime($post->post_date_gmt)),
+				'post_link' => get_permalink($post->ID),
+			];
+			$posts[] = array_merge($public);
+		}
+	}
 }
 
-if (!empty($args)) {
-	$context['posts'] = new Timber\PostQuery($args);
-}
-
-
-foreach ($context['posts'] as $post) {
-	$data = get_fields($post->ID);
-	$public = [
-		'post_title' => $post->post_title,
-		'post_img' => $data["post_thumb"]['url'],
-		'post_author_name' => get_the_author_meta($post->post_author),
-		'post_author_avatar' => get_avatar_url($post->post_author),
-		'post_category' => get_the_category($post->ID),
-		'post_desc' => mb_strimwidth($data["post_desc"], 0, 100, "..."),
-		'post_date' => date("d/m/Y H:i:s", strtotime($post->post_date_gmt)),
-		'post_link' => get_permalink($post->ID),
-	];
-	$posts[] = array_merge($public);
-}
 $context['posts'] = $posts;
 
-Timber::render($templates, $context);
+Timber::render(array('archive-' . $timber_post->post_type . '.twig', 'archive.twig'), $context);
