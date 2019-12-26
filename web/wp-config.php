0fb6f752188eb5e9d79350f50573310188024852
Isac Santana <koushirogamer@gmail.com>
2019-12-26 04:46:52 -0300

    Update wp-config.php

--- web/wp-config.php
+++ web/wp-config.php
@@ -4,6 +4,11 @@
  * This file is required in the root directory so WordPress can find it.
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
+
+ // FORCE SSL FOR ADMIN
+
+define('FORCE_SSL_ADMIN', true);
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
 require_once ABSPATH . 'wp-settings.php';
